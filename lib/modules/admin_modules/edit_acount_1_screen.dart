aeedd112950de05653ca8c485b857c62195c5f08
abdoelfky <aelfky66@gmail.com>
2022-12-02 02:09:45 +0200

    hard commit 2

--- lib/modules/admin_modules/edit_acount_1_screen.dart
+++ lib/modules/admin_modules/edit_acount_1_screen.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:teledoctor/modules/admin_modules/edit_account_screen.dart';
-import 'package:teledoctor/shared/component/components.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../shared/constants/constants.dart';
+
+
+
 
 class EditScreen1 extends StatefulWidget {
   const EditScreen1({Key? key}) : super(key: key);
@@ -96,104 +99,241 @@ class _EditScreen1State extends State<EditScreen1> {
   @override
   void initState() {
     // TODO: implement initState
-    super.initState();//addData();
+    super.initState();
+    addData();
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-            leading: BackButton(color: Colors.black,),
-            backgroundColor: Colors.white.withOpacity(.9),
-            title: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search,color: Colors.black,),
-                hintText: 'Search...',),
-              onChanged: (val) {
-                setState(() {
-                  name = val;
-                });
-              },
-            )),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance.collection('users').snapshots(),
-          builder: (context, snapshots) {
-            return (snapshots.connectionState == ConnectionState.waiting)
-                ? Center(
-              child: CircularProgressIndicator(),
-            )
-                : ListView.builder(
-                itemCount: snapshots.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var data = snapshots.data!.docs[index].data()
-                  as Map<String, dynamic>;
+        body:
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: blue5,
+                        borderRadius: BorderRadius.circular(25)),
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 25,
+                        )),
+                  ),
+                  Padding(
+                    padding:
+                    EdgeInsets.only(top: 7.0, left: size.width * .1),
+                    child: Text(
+                      'Search For Patient',
+                      style: TextStyle(
+                          color: primaryColor,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 22),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: '    Search... ',
+                  suffixIcon: Icon(
+                    Icons.search,
+                  ),
+                  suffixStyle: TextStyle(
+                      color: primaryColor
+                  ),
+                  suffixIconColor:primaryColor ,
 
-                  if (name.isEmpty) {
-                    return InkWell(
-                      onTap: (){
-                        navigateTo(context, EditAccountScreen());
-                      },
-                      child: ListTile(
-                        title: Text(
-                          data['name'],
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          data['email'],
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(data['image']),
-                        ),
-                      ),
-                    );
-                  }
-                  if (data['name']
-                      .toString()
-                      .toLowerCase()
-                      .startsWith(name.toLowerCase())) {
-                    return InkWell(
-                      onTap: (){
-                        navigateTo(context, EditAccountScreen());
-                      },
-                      child: ListTile(
-                        title: Text(
-                          data['name'],
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        subtitle: Text(
-                          data['email'],
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black54,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(data['image']),
-                        ),
-                      ),
-                    );
-                  }
-                  return Container();
-                });
-          },
+                  labelStyle: GoogleFonts.laila(
+                    fontSize: 22,
+                    color:primaryColor,
+                    fontStyle: FontStyle.italic,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(
+                      color:primaryColor,
+                      width:2,
+                    ),
+                  ),
+                  focusedBorder:OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide(
+                      color:primaryColor,
+                      width:3,
+                    ),
+                  ),
+
+
+                ),
+
+                onChanged: (val) {
+                  setState(() {
+                    name = val;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('users').snapshots(),
+                builder: (context, snapshots) {
+                  return (snapshots.connectionState == ConnectionState.waiting)
+                      ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                      : ListView.builder(
+                      itemCount: snapshots.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        var data = snapshots.data!.docs[index].data()
+                        as Map<String, dynamic>;
+
+                        if (name.isEmpty) {
+                          return InkWell(
+                            onTap: (){
+                            },
+                            child: Card(
+                              color: Colors.grey[100],
+
+                              elevation: 5,
+
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(15))),
+
+                              //  color: Colors.blue,
+
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  right: 12,
+                                  left: 12,
+                                ),
+                                child: Container(
+                                  child: Row(
+                                    children: [
+                                      //image
+                                      Container(
+                                        width: 80,
+                                        height: 85,
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.circular(6),
+                                            border: Border.all(
+                                              width: 6,
+                                              color: Colors.white,
+                                            )),
+                                        child: Center(
+                                          child: Image(
+                                            image: NetworkImage(data['image']),
+                                            fit: BoxFit.fill,
+                                            width: 100,
+                                            height: 100,
+                                          ),
+                                        ),
+                                      ),
+
+                                      SizedBox(
+                                        width: 10.0,
+                                      ),
+
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                              data['name'],
+                                              style: TextStyle(
+                                                  fontSize: 16.0, fontWeight: FontWeight.bold),
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                            Text(
+                                              'Enter Data : 25,Nov 2022',
+                                              style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 13.0,
+                                                fontWeight: FontWeight.w200,
+                                              ),
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  'Room No : 7',
+                                                  style: TextStyle(color: primaryColor),
+                                                ),
+                                                Spacer(),
+                                                TextButton(
+                                                  child: Text('Regiestered'),
+                                                  onPressed: () {
+
+                                                  },
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                        if (data['name']
+                            .toString()
+                            .toLowerCase()
+                            .startsWith(name.toLowerCase())) {
+                          return InkWell(
+                            onTap: (){
+                            },
+                            child: ListTile(
+                              title: Text(
+                                data['name'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle: Text(
+                                data['email'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(data['image']),
+                              ),
+                            ),
+                          );
+                        }
+                        return Container();
+                      });
+                },
+              ),
+            ),
+          ],
         ));
   }
 }
